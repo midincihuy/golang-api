d76d140294c0a05bb6a8e7ebbc7fd9c2420f7a07
hamidin.hidayat <hamidin.hidayat@moji.co.id>
2026-01-23 11:38:08 +0700

    function Get All Product and Create Product

--- main.go
+++ main.go
@@ -6,7 +6,42 @@ import (
 	"net/http"
 )
 
+type Produk struct {
+	ID int `json:"id"`
+	Nama string `json:"nama"`
+	Harga int `json:"harga"`
+	Stok int `json:"stok"`
+}
+
+var produk = []Produk{
+	{ID: 1, Nama: "Mie Ayam", Harga:15000, Stok:100},
+	{ID: 2, Nama: "Mie Bakso", Harga:15000, Stok:100},
+	{ID: 3, Nama: "Mie Ayam Bakso", Harga:20000, Stok:100},
+}
+
 func main(){
+
+	// GET Produk 
+	http.HandleFunc("/api/produk", func(w http.ResponseWriter, r *http.Request){
+		if r.Method == "GET" {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(produk)
+		} else if r.Method == "POST" {
+			var produkBaru Produk
+			err := json.NewDecoder(r.Body).Decode(&produkBaru)
+			if err != nil {
+				http.Error(w, "Invalid Request", http.StatusBadRequest)
+				return
+			}
+
+			produkBaru.ID = len(produk) + 1
+			produk = append(produk, produkBaru)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated) // 201
+			json.NewEncoder(w).Encode(produkBaru)
+		}
+	})
+
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{
